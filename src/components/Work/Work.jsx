09e9ac8e057026f2d2c2e7ae27d7af689efd33bf
Nand <studynand@18gmail.com>
2025-08-29 20:24:33 +0530

    project scroll fix

--- src/components/Work/Work.jsx
+++ src/components/Work/Work.jsx
@@ -5,7 +5,7 @@ import comingSoon from '../../assets/coming_soon.json';
 function Work() {
   return (
     <section
-      id="work"
+      id="projects"
       className="py-24 pb-24 px-[12vw] md:px-[7vw] lg:px-[20vw] font-sans relative"
     >
       {/* Section Title */}
