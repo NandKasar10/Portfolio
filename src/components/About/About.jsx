f87dbb1f3e0d4e8ff47e2a1a475cc1a9bb76d574
Nand <studynand@18gmail.com>
2026-04-16 18:09:54 +0530

    Resume added

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -59,6 +59,18 @@ function About() {
           >
             DOWNLOAD CV
           </a>
+          <a
+            href="https://drive.google.com/file/d/1yhIL0zJdttkzLqM00x5TikjVJf2E6lgl/view?usp=drive_link"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
+            style={{
+              background: "linear-gradient(90deg, #8245ec, #a855f7)",
+              boxShadow: "0 0 2px #8245ec, 0 0 2px #8245ec, 0 0 40px #8245ec",
+            }}
+          >
+            DOWNLOAD Resume
+          </a>
         </div>
         {/* Right Side */}
         <div className="md:w-1/2 flex justify-center md:justify-end">
